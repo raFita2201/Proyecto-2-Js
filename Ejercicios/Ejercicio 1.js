ca2238076bc7608675aed9836d95fcdfcb57c681
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2026-08-18 20:33:18 +0200

    update
    creacion de repo y primeros ejs

---
+++ Ejercicios/Ejercicio 1.js
@@ -0,0 +1,32 @@
+
+
+// 1. Define tu superhéroe favorito:
+
+let miSuperheroeFavorito = "Hulk"
+
+console.log(miSuperheroeFavorito)
+
+// 2. Define un número:
+
+const numero = 50
+
+console.log(numero)
+
+/*
+3. Define las dimensiones:
+
+Crea una variable llamada altura y asigna el valor 5.
+
+Crea una variable llamada longitud y asigna el valor 10.
+*/
+
+let altura = 5
+
+const longitud = 10
+
+console.log(altura , longitud)
+
+let suma = altura+longitud
+
+console.log(suma)
+
---
+++ Ejercicios/Ejercicio 2.js
@@ -0,0 +1,35 @@
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+jedi.edad = 25
+
+console.log(jedi)
+
+// 2. Presentación al estilo Leia Organa
+let nombre = "Leia";
+let apellido = "Organa";
+let edad = 20;
+
+console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan")
+
+// 3. Calculando el coste total de sables de luz
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+const costeTotal = sable1.precio + sable2.precio
+console.log(costeTotal)
+
+// 4. Actualizando el precio final de las naves
+let precioBaseGlobal = 10000;
+
+// Cambia el precio base en esta línea modificando la variable
+precioBaseGlobal = 25000;
+
+const nave1 = { nombre: "Ala-X", precioBase: 50000, precioFinal: 60000 };
+const nave2 = { nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000 };
+
+// Tu código para actualizar el precio final de cada nave aquí...
+nave1.precioFinal = nave1.precioBase + precioBaseGlobal;
+nave2.precioFinal = nave2.precioBase + precioBaseGlobal;
+
+console.log(nave1);
+console.log(nave2);
